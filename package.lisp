99bbbbf002861009b143d64af5ec4e611e1bddd7
Wojciech S. Gac <wojciech@iagon.com>
2022-12-04 23:18:45 +0100

    Pre-define packages for the remaining days

--- package.lisp
+++ package.lisp
@@ -19,3 +19,89 @@
 (defpackage #:advent-of-code-2022.day04
   (:nicknames #:aoc2022.day04)
   (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day05
+  (:nicknames #:aoc2022.day05)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day06
+  (:nicknames #:aoc2022.day06)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day07
+  (:nicknames #:aoc2022.day07)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day08
+  (:nicknames #:aoc2022.day08)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day09
+  (:nicknames #:aoc2022.day09)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day10
+  (:nicknames #:aoc2022.day10)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day11
+  (:nicknames #:aoc2022.day11)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day12
+  (:nicknames #:aoc2022.day12)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day13
+  (:nicknames #:aoc2022.day13)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day14
+  (:nicknames #:aoc2022.day14)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day15
+  (:nicknames #:aoc2022.day15)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day16
+  (:nicknames #:aoc2022.day16)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day17
+  (:nicknames #:aoc2022.day17)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day18
+  (:nicknames #:aoc2022.day18)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day19
+  (:nicknames #:aoc2022.day19)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day20
+  (:nicknames #:aoc2022.day20)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day21
+  (:nicknames #:aoc2022.day21)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day22
+  (:nicknames #:aoc2022.day22)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day23
+  (:nicknames #:aoc2022.day23)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day24
+  (:nicknames #:aoc2022.day24)
+  (:use #:cl #:util))
+
+(defpackage #:advent-of-code-2022.day25
+  (:nicknames #:aoc2022.day25)
+  (:use #:cl #:util))
+
+
